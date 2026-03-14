65c75d59f420645948323fdd7a4b920d029cfb93
eavan127 <eavantan9@gmail.com>
2026-03-14 11:42:34 +0800

    fix: remove vs files

--- Assets/Book_Navigation_Script.cs
+++ Assets/Book_Navigation_Script.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Book_Navigation_Script : MonoBehaviour
 {
+    public string sceneToLoad;
+
     void OnMouseDown()
     {
-        Debug.Log("CLICK DETECTED");
+        Debug.Log("Loading scene: " + sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
-
